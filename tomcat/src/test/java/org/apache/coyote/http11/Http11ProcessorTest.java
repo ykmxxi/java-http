94507e4b5aac04c0e5ac910f5da997089e692dd0
ykmxxi <ykmxxi97@gmail.com>
2025-09-10 01:15:25 +0900

    test: 회원가입 GET, POST 테스트

--- tomcat/src/test/java/org/apache/coyote/http11/Http11ProcessorTest.java
+++ tomcat/src/test/java/org/apache/coyote/http11/Http11ProcessorTest.java
@@ -112,4 +112,56 @@ class Http11ProcessorTest {
         assertThat(result).contains("Location: /index.html ");
         assertThat(result).contains("Set-Cookie: JSESSIONID=");
     }
+
+    @Test
+    void getRegister() throws IOException {
+        // given
+        final String httpRequest= String.join("\r\n",
+            "GET /register HTTP/1.1 ",
+            "Host: localhost:8080 ",
+            "Connection: keep-alive ",
+            "",
+            "");
+
+        final var socket = new StubSocket(httpRequest);
+        final Http11Processor processor = new Http11Processor(socket);
+
+        // when
+        processor.process(socket);
+
+        // then
+        final URL resource = getClass().getClassLoader().getResource("static/register.html");
+        var expected = "HTTP/1.1 200 OK \r\n" +
+            "Content-Type: text/html;charset=utf-8 \r\n" +
+            "Content-Length: 4319 \r\n" +
+            "\r\n"+
+            new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
+
+        assertThat(socket.output()).isEqualTo(expected);
+    }
+
+    @Test
+    void postRegisterSuccess() {
+        // given
+        final String httpRequest= String.join("\r\n",
+            "POST /login HTTP/1.1 ",
+            "Host: localhost:8080 ",
+            "Connection: keep-alive ",
+            "Content-Length: 80 ",
+            "Content-Type: application/x-www-form-urlencoded ",
+            "Accept: */*",
+            "",
+            "account=gugu&password=password&email=hkkang%40woowahan.com");
+
+        final var socket = new StubSocket(httpRequest);
+        final Http11Processor processor = new Http11Processor(socket);
+
+        // when
+        processor.process(socket);
+
+        // then
+        final var result = socket.output();
+        assertThat(result).contains("HTTP/1.1 302 Found ");
+        assertThat(result).contains("Location: /index.html ");
+    }
 }
